d86b409def0cfd827b1e6e7e403e7eb52ebc892f
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-12-24 01:25:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,121 @@
 import requests
+from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Add your Telegram bot token here
+TELEGRAM_BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 # Stylish logo for BINof
 def binof_logo():
     return '''
     
+    
     $$\   $$\           $$\           $$\                           
     $$ | $$  |          \__|          $$ |                          
     $$ |$$  /  $$$$$$\  $$\  $$$$$$$\ $$$$$$$\  $$$$$$$\   $$$$$$\  
-    $$$$$  /  $$  __$$\ $$ |$$  _____|$$  __$$\ $$  __$$\  \____$$\ 
-    $$  $$<   $$ |  \__|$$ |\$$$$$$\  $$ |  $$ |$$ |  $$ | $$$$$$$ |
-    $$ |\$$\  $$ |      $$ | \____$$\ $$ |  $$ |$$ |  $$ |$$  __$$ |
+    $$$$$  /  $$  $$\ $$ |$$  _____|$$  __$$\ $$  __$$\  \____$$\ 
+    $$  $$<   $$ |  \|$$ |\$$$$$$\  $$ |  $$ |$$ |  $$ | $$$$$$$ |
+    $$ |\$$\  $$ |      $$ | \____$$\ $$ |  $$ |$$ |  $$ |$$  $$ |
     $$ | \$$\ $$ |      $$ |$$$$$$$  |$$ |  $$ |$$ |  $$ |\$$$$$$$ |
-    \__|  \__|\__|      \__|\_______/ \__|  \__|\__|  \__| \_______|
+    \|  \__|\__|      \__|\_______/ \__|  \__|\__|  \__| \_______|
     '''
 
 def is_vbv(card_number):
     vbv_indicator = card_number[0:6]
     vbv_bins = [
-    "400115", "400117", "400118", "400119", "400135", "400136", "400137", "400138",
-    "400139", "400175", "400344", "400345", "400346", "400347", "400348", "400349",
-    "400350", "400351", "400352", "400353", "400354", "400355", "400356", "400357",
-    "400358", "400359", "400360", "400361", "400362", "400363", "400364", "400365",
-    "400366", "400367", "400368", "400369", "400370", "400371", "400372", "400373",
-    "400374", "400375", "400376", "400377", "400378", "400379", "400380", "400381",
-    # Add more VBV BINs here
-]
-
+        "400115", "400117", "400118", "400119", "400135", "400136", "400137", "400138",
+        "400139", "400175", "400344", "400345", "400346", "400347", "400348", "400349",
+        "400350", "400351", "400352", "400353", "400354", "400355", "400356", "400357",
+        "400358", "400359", "400360", "400361", "400362", "400363", "400364", "400365",
+        "400366", "400367", "400368", "400369", "400370", "400371", "400372", "400373",
+        "400374", "400375", "400376", "400377", "400378", "400379", "400380", "400381",
+        # Add more VBV BINs here
+    ]
     
     return vbv_indicator in vbv_bins
 
-def get_bin_info(bin_number):
+def get_bin_info(bin_number, update):
     url = f"https://lookup.binlist.net/{bin_number}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
             bin_info = response.json()
             if bin_info:
-                print(f"+++++++++++++++++BIN: {bin_number}")
-                print(f"+++++++++++++++++Length: {bin_info.get('number', {}).get('length', 'N/A')}")
-                print(f"+++++++++++++++++Card Type: {bin_info.get('type', 'N/A')}")
-                print(f"+++++++++++++++++Brand: {bin_info.get('brand', 'N/A')}")
-                print(f"+++++++++++++++++Country: {bin_info.get('country', {}).get('name', 'N/A')} {bin_info.get('country', {}).get('emoji', 'N/A')}")
-                print(f"+++++++++++++++++Bank Name: {bin_info.get('bank', {}).get('name', 'N/A')}")
-                print(f"+++++++++++++++++Bank URL: {bin_info.get('bank', {}).get('url', 'N/A')}")
-                print(f"+++++++++++++++++Bank Phone: {bin_info.get('bank', {}).get('phone', 'N/A')}")
-                print(f"+++++++++++++++++Bank City: {bin_info.get('bank', {}).get('city', 'N/A')}")
-                print()
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++BIN: {bin_number}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Length: {bin_info.get('number', {}).get('length', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Card Type: {bin_info.get('type', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Brand: {bin_info.get('brand', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Country: {bin_info.get('country', {}).get('name', 'N/A')} {bin_info.get('country', {}).get('emoji', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Bank Name: {bin_info.get('bank', {}).get('name', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Bank URL: {bin_info.get('bank', {}).get('url', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Bank Phone: {bin_info.get('bank', {}).get('phone', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text=f"+++++++++++++++++Bank City: {bin_info.get('bank', {}).get('city', 'N/A')}")
+                bot.send_message(chat_id=update.effective_chat.id, text="")
             else:
-                print("Invalid BIN Input !!")
+                bot.send_message(chat_id=update.effective_chat.id, text="Invalid BIN Input !!")
         else:
-            print("Invalid BIN Input !!")
+            bot.send_message(chat_id=update.effective_chat.id, text="Invalid BIN Input !!")
     except requests.exceptions.RequestException:
-        print("Error: Unable to retrieve BIN information. Please check your internet connection.")
+        bot.send_message(chat_id=update.effective_chat.id, text="Error: Unable to retrieve BIN information. Please check your internet connection.")
     except ValueError:
-        print("Error: Invalid response from the BIN information service.")
+        bot.send_message(chat_id=update.effective_chat.id, text="Error: Invalid response from the BIN information service.")
 
-def mass_bin_info(file_path):
+def mass_bin_info(file_path, update):
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 bin_number = line.strip()
-                get_bin_info(bin_number)
+                get_bin_info(bin_number, update)
                 if is_vbv(bin_number):
-                    print("+++++++++++++++++ This card is VBV.")
+                    bot.send_message(chat_id=update.effective_chat.id, text="+++++++++++++++++ This card is VBV.")
                 else:
-                    print("$$$$$$$$$$$$$$$--- This card is non-VBV.")
+                    bot.send_message(chat_id=update.effective_chat.id, text="$$$$$$$$$$$$$$$--- This card is non-VBV.")
     except FileNotFoundError:
-        print("Error: The specified file does not exist.")
+        bot.send_message(chat_id=update.effective_chat.id, text="Error: The specified file does not exist.")
     except IOError:
-        print("Error: Unable to read the file. Please check the file path.")
+        bot.send_message(chat_id=update.effective_chat.id, text="Error: Unable to read the file. Please check the file path.")
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(binof_logo())
+    update.message.reply_text("""
+    1. Mass BIN Check Mode
+    2. Single Bin Check Mode
+    3. Exit
+    """)
+
+# ...
+
+def handle_text(update: Update, context: CallbackContext):
+    text = update.message.text
+    if text == '1':
+        update.message.reply_text("Enter the path to the BIN file:")
+        context.user_data['mode'] = 'mass_bin'
+    elif text == '2':
+        update.message.reply_text("Enter the BIN to check:")
+        context.user_data['mode'] = 'single_bin'
+    elif text == '3':
+        update.message.reply_text("Exiting ....")
+        context.user_data['mode'] = 'exit'
+    else:
+        update.message.reply_text("Invalid choice. Please enter a valid option.")
 
 def main():
+    updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
+
+    updater.start_polling()
+
     try:
-        while True:
-            print(binof_logo())
-            print("""
-1. Mass BIN Check Mode
-2. Single Bin Check Mode
-3. Exit
-            """)
-            opt = input("$ Enter Your Mode : ")
-            if opt == '1':
-                path = input("$ Enter The Bin file's Path : ")
-                mass_bin_info(path)
-            elif opt == '2':
-                bin_number = input("$ Enter the BIN to Check : ")
-                get_bin_info(bin_number)
-                if is_vbv(bin_number):
-                    print("+++++++++++++++++ This card is VBV.")
-                else:
-                    print("$$$$$$$$$$$$$$$---This card is non-VBV.")
-            elif opt == '3':
-                break
-            else:
-                print("Invalid choice. Please enter a valid option.")
+        updater.idle()
     except KeyboardInterrupt:
         print("\nExiting ....")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+    
